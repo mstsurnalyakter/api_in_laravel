d6680fdb71f8004c8102656aefb4c530d8919db7
mstsurnalyakter <surnalyms@gmail.com>
2024-12-25 00:21:20 -0700

    refactor API routes to group authenticated endpoints and improve organization

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,31 @@ use App\Http\Controllers\UsersAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::get('/test', function(){
-    return "Test route is working";
-});
+// Route::get('/test', function(){
+//     return "Test route is working";
+// });
 
-Route::post('add-student', [StudentsController::class, 'addStudent']);
-Route::get('students', [StudentsController::class, 'list']);
-Route::put('update-student/{id}', [StudentsController::class, 'updateStudent']);
-Route::delete('delete-student/{id}',[StudentsController::class,'deleteStudent']);
-Route::get('search-student/{studentname}',[StudentsController::class,'searchStudent']);
-
-
-// resource controller
-Route::resource('members',MemberController::class);
 
 // user authentication
 Route::post('signup', [UsersAuthController::class, 'signup']);
 Route::post('login', [UsersAuthController::class, 'login']);
+
+Route::group(["middleware"=>["auth:sanctum"]],function(){
+    Route::post('add-student', [StudentsController::class, 'addStudent']);
+    Route::get('students', [StudentsController::class, 'list']);
+    Route::put('update-student/{id}', [StudentsController::class, 'updateStudent']);
+    Route::delete('delete-student/{id}',[StudentsController::class,'deleteStudent']);
+    Route::get('search-student/{studentname}',[StudentsController::class,'searchStudent']);
+
+    // resource controller
+Route::resource('members',MemberController::class);
+    
+});
+
+
+
+
